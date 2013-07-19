92f8f95dfd00428be8a04e0d21570052c796186a
Ubuntu <ubuntu@ip-10-98-135-83.ec2.internal>
2013-07-19 19:33:08 +0000

    run on AWS

--- web.js
+++ web.js
@@ -1,17 +1,13 @@
 var express = require('express');
 var path = require('path');
 
-
 var app = express.createServer(express.logger());
 
-app.get('/', function(request, response) {
-	var filePath = path.normalize(__dirname + "/index.html");
-    response.sendfile(filePath, {request: request});
-
-//  response.send('Hello World 2!');
+app.configure(function () {
+	app.use(express.static(path.join(__dirname, 'static')));
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
